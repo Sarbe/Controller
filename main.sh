bddf59d45a40eacbff1f2aab7421c6d82c0301d1
Sarbe <sarbe97@gmail.com>
2018-08-18 16:29:21 +0530

    first

---
+++ main.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+WRK_DIR=/home/sarbe/POS/src
+
+GIT_DIR=Rxdfert
+#1.go to working folder
+cd $WRK_DIR||exit
+
+if [ -d $WRK_DIR/Rxdfert ] 
+then
+	rm -r -f
+fi
+
+echo 'Cloning Repository from GIT for first time.'
+git clone https://github.com/Sarbe/Rxdfert.git
+
+#3.. go to Rxdfert folder
+cd $WRK_DIR/Rxdfert||exit
+
+#4. mvn install
+mvn clean install
+
+#4 copy jar to src folder
+cp *.jar $WRK_DIR/POS-Retailer.jar
+
+
+
+
